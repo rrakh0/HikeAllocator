9ed1a0daa26e031153ff0471241f34f7031b2dc0
Rinat Rakhmankulov <rinat_rakhmankulov@epam.com>
2021-06-19 12:47:48 +0400

    Added convert to table. Refactoring

--- allocator.js
+++ allocator.js
@@ -1,14 +1,9 @@
-let example = require('./examples/05.example.json');
+let hikeData = require('./examples/02.example.json');
 let { sum, copy, deepCopy, sort } = require('./utils.js')();
 
-const allocate = (hikeData) => {
+const allocate = ({ participants, items }) => {
 
-    let items = deepCopy(hikeData.items || []);
-    let participants = deepCopy(hikeData.participants || [])
-
-    items.forEach(item => item.work = calcWork(item));
-    sort(items, (item) => item.work);
-    calcExpectedWork(participants, items)
+    prepareData({ participants, items });
 
     return allocateInternal(participants, items, items.length - 1);
 };
@@ -48,6 +43,25 @@ const allocateInternal = (participants, items, index) => {
     return allocations;
 };
 
+const prepareData = ({ participants, items }) => {
+
+    items.forEach(item => item.work = calcWork(item));
+    sort(items, (item) => item.work);
+    calcExpectedWork(participants, items);
+};
+
+const calcWork = (item) => {
+    switch (item.consumptionType) {
+        case 'Fixed': {
+            return item.weight * item.carryDays;
+        }
+        case 'Linear': {
+            return item.weight * (item.carryDays + 1) / 2;
+        }
+    }
+    throw new Error(`${item.consumptionType} is not supported`);
+};
+
 const calcExpectedWork = (participants, items) => {
 
     let totalWork = sum(items, (item) => item.work);
@@ -59,66 +73,98 @@ const calcExpectedWork = (participants, items) => {
     }
 };
 
-const calcWork = (item) => {
-    switch (item.consumptionType) {
-        case 'Fixed': {
-            return item.weight * item.durationDays;
-        }
-        case 'Linear': {
-            return item.weight * (item.durationDays + 1) / 2;
-        }
-    }
-    throw new Error(`${item.consumptionType} is not supported`);
-};
-
 const calcTotalDiff = (allocations) => {
 
-    let result = 0;
-
-    for (let alloc of allocations) {
-        result += Math.pow(alloc.participant.expectedWork - sum(alloc.items, i => i.work), 2);
-    }
-
-    return result;
+    return sum(allocations, a => Math.pow(a.participant.expectedWork - sum(a.items, i => i.work), 2));
 };
 
 const calcMetrics = (allocations) => {
 
-    for (let alloc of allocations) {
-        alloc.actualWork = sum(alloc.items, i => i.work).toFixed(1);
-        alloc.diffPct = (100 * (alloc.participant.expectedWork - alloc.actualWork) / alloc.participant.expectedWork).toFixed(2);
-        alloc.diff = (alloc.participant.expectedWork - alloc.actualWork).toFixed(1);
-        alloc.weight = sum(alloc.items, i => i.weight).toFixed(1);
-
-        
-    }
+    allocations.forEach(a => {
+        a.actualWork = sum(a.items, i => i.work).toFixed(1);
+        a.diffPct = (100 * (a.participant.expectedWork - a.actualWork) / a.participant.expectedWork).toFixed(2);
+        a.diff = (a.participant.expectedWork - a.actualWork).toFixed(1);
+        a.weight = sum(a.items, i => i.weight).toFixed(1);
+    })
 };
 
 const sortItems = (allocations) => {
 
-    for (let alloc of allocations) {
-        sort(alloc.items, (item) => -item.durationDays)
-    }
+    allocations.forEach(a => {
+        sort(a.items, (item) => -item.carryDays)
+    })
 };
 
 const getEquipmentCount = (items) => {
+
     return items.filter(i => i.type == 'Equipment').length;
+};
+
+const getItemDisplayName = (item) => {
+
+    let suffix = isDailyFoodItem(item) ? ' ' + (item.consumptionDay ?? item.carryDays) : '';
+
+    return `${item.name}${suffix}(${item.weight})`;
 }
 
+const getItemsDisplay = (items) => {
+
+    return items.map(i => getItemDisplayName(i)).join('; ');
+}
+
+const isDailyFoodItem = (item) => {
+
+    return item.type == 'Food' && item.consumptionType == 'Fixed';
+}
+
+const convertToTable = (allocations, days) => {
+
+    let table = {};
+
+    table['Header'] = ['Снаряж', 'Общ. продукт'];
+    let header = table['Header'];
+
+    for (let i = 0; i < days; i++) header.push('День ' + (i + 1));
+    header.push('Вес');
+    header.push('Работа');
+    header.push('Дифф');
+
+    for (let a of allocations) {
+
+        table[a.participant.name] = [];
+        let row = table[a.participant.name];
+
+        row.push(getItemsDisplay(a.items.filter(i => i.type == 'Equipment')));
+        row.push(getItemsDisplay(a.items.filter(i => i.consumptionType == 'Linear')));
+
+        for (let i = 0; i < days; i++) {
+            row.push(getItemsDisplay(a.items.filter(t => isDailyFoodItem(t) && (t.consumptionDay || t.carryDays) == (i + 1))));
+        }
+
+        row.push(a.weight);
+        row.push(a.actualWork);
+        row.push(`${a.diff} (${a.diffPct} %)`);
+    }
+
+    return table;
+};
+
 const run = () => {
-    let allocations = allocate(example);
+    let allocations = allocate(hikeData);
 
     calcMetrics(allocations);
     sortItems(allocations);
 
     allocations.map(a => ({
         name: a.participant.name,
-        items: JSON.stringify(a.items.map(i => i.key)),
+        items: JSON.stringify(a.items.map(i => getItemDisplayName(i))),
         expectedWork: a.participant.expectedWork.toFixed(1),
         actualWork: a.actualWork,
         diff: `${a.diff} (${a.diffPct} %)`,
         weight: a.weight,
     })).forEach(a => console.table(a));
+
+    console.table(convertToTable(allocations, hikeData.durationDays));
 };
 
 run();
